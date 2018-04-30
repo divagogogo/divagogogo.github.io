25987ba2483e3b6e7e24ab9e607ddf58dfbf2676
cugwyman <435059696@qq.com>
2018-04-30 10:47:18 +0800

    feat：删除photoList注释

--- src/views/photoList.vue
+++ src/views/photoList.vue
@@ -105,7 +105,7 @@ margin-bottom: 40px;
         </div>
       </div>
     </div>
-    <br><br><br><br><br><br><br><br><br><br> /*To fix Safari problem*/
+    <br><br><br><br><br><br><br><br><br><br>
   </div>
 </div>
 </template>
